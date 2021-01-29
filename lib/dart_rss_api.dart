d85244acf75ae67d33a5f334bb06a361cb52f26b
Marko Korhonen <marko@korhonen.cc>
2021-01-29 17:43:52 +0200

    Initial commit: create project with stagehand

---
+++ lib/dart_rss_api.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library dart_rss_api;
+
+export 'src/dart_rss_api_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
